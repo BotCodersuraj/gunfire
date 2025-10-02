8d40f4becd139b4903ab83abb06ccb11e519e0e3
CreatorZorvaxo <moharsingh7221@gmail.com>
2025-10-02 14:41:10 +0530

    Update fetchinfo.js

--- api/fetchinfo.js
+++ api/fetchinfo.js
@@ -1,11 +1,12 @@
-// /pages/api/tournament.js
+// /pages/api/fetchinfo.js
 
 let participants = [];
 let lastReset = Date.now();
 
 export default function handler(req, res) {
   const now = Date.now();
-  // reset after 24h
+
+  // Reset every 24h
   if (now - lastReset > 24 * 60 * 60 * 1000) {
     participants = [];
     lastReset = now;
@@ -13,7 +14,7 @@ export default function handler(req, res) {
 
   const { phone_number, ffuid } = req.query;
 
-  // Agar join request aayi hai
+  // Agar join karne ke liye query aayi hai
   if (phone_number && ffuid) {
     const userjoinnumber = participants.length + 1;
     const entry = {
@@ -32,7 +33,7 @@ export default function handler(req, res) {
     });
   }
 
-  // Agar sirf list fetch karna hai
+  // Agar sirf fetch karna hai
   return res.status(200).json({
     participants,
     total: participants.length
